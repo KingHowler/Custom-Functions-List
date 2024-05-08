d3d3f2de18762fbd923c34cca1ab4515931cae3b
KingHowler <68814294+KingHowler@users.noreply.github.com>
2024-05-08 20:01:34 +0500

    Update DPI Aware.cs

--- DPI Aware Mod/DPI Aware.cs
+++ DPI Aware Mod/DPI Aware.cs
@@ -1,7 +1,3 @@
-// In Form's properties, Set Auto Scalling to DPI
-// Then in program.cs replace static void main() with the following code
-// Note: In line 6, Replace Form1 with the name of the first winform the program runs
-
 static void Main()
 {
     if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
