ce2bc87e02db96421900038d44a9dc7cdfb07658
KingHowler <68814294+KingHowler@users.noreply.github.com>
2024-05-09 16:55:40 +0500

    Update primeTest.cpp

--- primeTest/primeTest.cpp
+++ primeTest/primeTest.cpp
@@ -1,18 +1,17 @@
 #include <cmath>
 
 bool primeTest (int Number) {
-  int CheckNum = static_cast<int>(sqrt(Number));
+  if (Number == 2 || Number == 3) {return true;}
+  if (Number <= 1) {return false;}
+  if ((Number % 2) == 0 || (Number % 3) == 0) {return false;}
 
-  if ( (Number <= 1) || ( ( ( (Number % 2) == 0) || ( (Number % 3) == 0) ) && ( (Number !=3) || (Number != 2) ) ) ) {
-    return false;
-  }
-  if (Number <= 3) {return true;}
   else {
+    int CheckNum = static_cast<int>(sqrt(Number));
+
     for (int c = 5; c <= CheckNum; c = c + 6) {
-      if ((Number % c) == 0 || ((Number % (c+2)) == 0)) {
-        return false;
-      }
+      if ((Number % c) == 0 || ((Number % (c+2)) == 0)) {return false;}
     }
   }
+  
   return true;
 }
