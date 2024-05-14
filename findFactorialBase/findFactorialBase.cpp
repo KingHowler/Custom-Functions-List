9ec8d6920d041d30894ce5d9277395b5c11fbee6
KingHowler <68814294+KingHowler@users.noreply.github.com>
2024-05-14 17:25:12 +0500

    Update findFactorialBase.cpp

--- findFactorialBase/findFactorialBase.cpp
+++ findFactorialBase/findFactorialBase.cpp
@@ -1,10 +1,10 @@
 int findFactorialBase(int factorial) {
   if (factorial < 1) {return -1;}
   int base = 1;
-  if (factorial % base != 0) {return -1;}
 
   
   do {
+    if (factorial % base != 0) {return -1;}
     factorial = factorial/base;
     base = base + 1;
   } while(factorial > base );
