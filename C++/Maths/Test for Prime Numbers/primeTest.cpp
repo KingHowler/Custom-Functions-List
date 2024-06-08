1211ed801145c6fe4a4295fde8401d2d675ed10c
M. Umar Shahbaz <68814294+KingHowler@users.noreply.github.com>
2024-06-09 00:33:35 +0500

    Updated primeTest.cpp

--- C++/Maths/Test for Prime Numbers/primeTest.cpp
+++ C++/Maths/Test for Prime Numbers/primeTest.cpp
@@ -1,17 +1,30 @@
 #include <cmath>
 
-bool primeTest (int Number) {
-  if (Number == 2 || Number == 3) {return true;}
-  if (Number <= 1) {return false;}
-  if ((Number % 2) == 0 || (Number % 3) == 0) {return false;}
+bool primeTest(int Number)
+{
+  // Exceptions
+  if (Number == 2 || Number == 3) // Check if number is 2 or 3 before checking if the number is a multiple of 2 or 3
+  {
+    return true;
+  }
+  if ((Number % 2) == 0 || (Number % 3) == 0) // This check is done before main processing to reduce expressions of possible factors in the main processing
+  {
+    return false;
+  }
+  if (Number <= 1) // Number less than 1 are outside of the parameters
+  {
+    return false;
+  }
 
-  else {
-    int CheckNum = static_cast<int>(sqrt(Number));
-
-    for (int c = 5; c <= CheckNum; c = c + 6) {
-      if ((Number % c) == 0 || ((Number % (c+2)) == 0)) {return false;}
+  // Main Code
+  int CheckNum = static_cast<int>(sqrt(Number)); // The limit for calculation, beyond this point all calculations are unnecesaary
+  for (int c = 5; c <= CheckNum; c = c + 6)      // Count controlled loop to perfom checks in terms of c + n (which is equivalent to 6k + n where k increments by one each time)
+  {
+    if ((Number % c) == 0 || ((Number % (c + 2)) == 0)) // The previous checks for 2 and 3 removed all factors leaving only c and c + 2 as expressions for all possible prime factors
+    {
+      return false;
     }
   }
 
-  return true;
+  return true; // If Number doesn`t fulfill any of the conditions above then it must be a prime number
 }
