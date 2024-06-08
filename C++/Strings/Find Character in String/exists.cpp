220479cdc379f2797c1865d94b48f5fd7be8d403
M. Umar Shahbaz <68814294+KingHowler@users.noreply.github.com>
2024-06-09 00:53:01 +0500

    Updated exists.cpp

--- C++/Strings/Find Character in String/exists.cpp
+++ C++/Strings/Find Character in String/exists.cpp
@@ -1,10 +1,15 @@
 #include <string>
 
-bool exists(std::string text, char letter) {
-
-  for (int i = 0; i < text.length(); i++) {
-    if (text[i] == letter) {return true;}
+bool exists(std::string text, char letter)
+{
+  // there are no exceptions, all incorrect parameters will result in runtime or syntax errors
+  for (int i = 0; i < text.length(); i++)
+  { // compare the inputted charater with each charater in the string
+    if (text[i] == letter)
+    {
+      return true;
+    } // if they match return true
   }
 
-  return false;
+  return false; // if they don't match return false
 }
