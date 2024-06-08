b30882015f266c2037774e4614d33b463e9f4387
M. Umar Shahbaz <68814294+KingHowler@users.noreply.github.com>
2024-06-09 00:49:22 +0500

    Updated generatePass.cpp

--- C++/Strings/Generate Password/generatePass.cpp
+++ C++/Strings/Generate Password/generatePass.cpp
@@ -1,32 +1,42 @@
-#include "randomChar.cpp"
+#include "randomChar.cpp" // include the functions : randomChar, randomCharInit, exists
 #include "exists.cpp"
 
-void generatePassInit() {randomCharInit();}
+void generatePassInit() { randomCharInit(); } // use generatePassInit to seed the generator
 
-std::string generatePass(int length, int mode, bool duplicate) {
-  if (duplicate == false) {
-    if (mode != 2 && length > 26) {return "Error : Can`t generate enough unique characters";}
-    if (mode == 2 && length > 52) {return "Error : Can`t generate enough unique characters";}
+std::string generatePass(int length, int mode, bool duplicate)
+{ // take the length, generator mode, and boolean for duplicated characters check as input
+  if (duplicate == false)
+  { // return -1 if there aren't enough characters to create unique password
+    if (mode != 2 && length > 26)
+    {
+      return "-1";
     }
+    if (mode == 2 && length > 52)
+    {
+      return "-1";
+    }
+  }
 
   std::string pass;
-  char buff;
 
-  for (int l = 0; l < length; l++) {
+  for (int l = 0; l < length; l++)
+  {
 
-    if (duplicate == true) {
+    if (duplicate == true)
+    { // if duplicated characters are allowed, concatenate random characters directly
       pass = pass + randomChar(mode);
     }
 
-    else {
-
-      do {
+    else
+    { // if duplicated characters are not allowed, check each random character before concatenation
+      char buff;
+      do
+      {
         buff = randomChar(mode);
-      } while(exists(pass, buff));
+      } while (exists(pass, buff));
 
       pass = pass + buff;
-
     }
   }
-  return pass;
+  return pass; // return the password generated
 }
