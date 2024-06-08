e2b3b542c1f7ad35c0cf58e6c95a43034a80ff4f
M. Umar Shahbaz <68814294+KingHowler@users.noreply.github.com>
2024-06-09 00:35:06 +0500

    Updated randomChar.cpp

--- C++/Strings/Random Character Generator/randomChar.cpp
+++ C++/Strings/Random Character Generator/randomChar.cpp
@@ -1,19 +1,28 @@
 #include <cstdlib>
 #include <ctime>
 
-void randomCharInit() { std::srand(static_cast<unsigned int>(std::time(nullptr))); }
+char Characters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; // This array contains all possible characters that can be outputed
 
+void randomCharInit() { std::srand(static_cast<unsigned int>(std::time(nullptr))); } // Function to seed the generator, this ensures the generated character is unique each time
 
-char randomChar(int mode) {
-
-  char Characters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-  switch (mode) {
-    case 0: return Characters[std::rand() % 26]; break;
-    case 1: return Characters[(std::rand() % 26) + 26]; break;
-    case 2: return Characters[std::rand() % 52]; break;
+char randomChar(int mode)
+{
+  switch (mode)
+  {
+  case 0: //  lowercase generator mode
+    return Characters[std::rand() % 26];
+    break;
+  case 1: // UPPERCASE generator mode
+    return Characters[(std::rand() % 26) + 26];
+    break;
+  case 2: //  boTH generator mode
+    return Characters[std::rand() % 52];
+    break;
+  default: // Incorrect mode/parameter
+    return '*';
+    break;
   }
 }
